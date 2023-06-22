f9154ffe3cbc8de27337f46ff9efdf1eb8c51f70
Surin Lee <leesurin99@gmail.com>
2023-06-22 15:17:31 +0900

    first commit

---
+++ src/main/java/com/example/test_mysql_github_action/TestMysqlGithubActionApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_mysql_github_action;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMysqlGithubActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestMysqlGithubActionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/test_mysql_github_action/TestMysqlGithubActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_mysql_github_action;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMysqlGithubActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
